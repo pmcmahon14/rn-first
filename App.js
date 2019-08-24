88d8e68d4098608eca679edf8c67e3b81f3cb416
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-24 13:55:59 -0700

    added flexbox boxes for styling experimentation

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
+      <View>
     <View style={{padding: 60}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         <TextInput
             placeholder='Course Goal'
-            style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10}}
+            style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
         />
         <Button title='ADD'/>
       </View>
@@ -15,6 +16,49 @@ export default function App() {
 
       </View>
     </View>
+          <View
+              style={{
+                  padding: 50,
+                  flexDirection: 'row',
+                  width: '80%',
+                  height: 300,
+                  justifyContent: 'space-around',
+                  alignItems: 'stretch',
+              }}
+          >
+              <View
+                  style={{
+                      backgroundColor: 'red',
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                  }}
+              >
+                  <Text>1</Text>
+              </View>
+              <View
+                  style={{
+                      backgroundColor: 'blue',
+                      flex: 2,
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                  }}
+              >
+                  <Text>2</Text>
+              </View>
+              <View
+                  style={{
+                      backgroundColor: 'green',
+                      flex: 3,
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                  }}
+              >
+                  <Text>3</Text>
+              </View>
+          </View>
+      </View>
+
   );
 }
 
