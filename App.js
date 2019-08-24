ab2a9e8b191cba53f7f3ddc39b26bf5dbc9c975b
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-24 12:27:06 -0700

    first flexbox design

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 60}}>
-      <View>
-        <TextInput placeholder='Course Goal' style={{borderBottomColor: 'black', borderBottomWidth: 1}}/>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput
+            placeholder='Course Goal'
+            style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10}}
+        />
         <Button title='ADD'/>
       </View>
       <View>
