a09f83d51ec76c488de7942e93532547bd48423d
Patrick McMahon <pmcmahon14@gmail.com>
2019-09-15 21:12:35 -0700

    added styling to list items

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
                 <Button title="ADD" onPress={addGoalHandler} />
             </View>
             <View>
-                {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+                {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
             </View>
         </View>
     );
@@ -45,5 +45,12 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         padding: 10
+    },
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
     }
 });
