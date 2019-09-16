f0645d073e556cc6e31b173ad5cebc07d5ef7768
Patrick McMahon <pmcmahon14@gmail.com>
2019-09-15 20:48:11 -0700

    outputting list of todo items

--- App.js
+++ App.js
@@ -1,72 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
-      <View>
-    <View style={{padding: 60}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput
-            placeholder='Course Goal'
-            style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
-        />
-        <Button title='ADD'/>
-      </View>
-      <View>
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
 
-      </View>
-    </View>
-          <View
-              style={{
-                  padding: 50,
-                  flexDirection: 'row',
-                  width: '80%',
-                  height: 300,
-                  justifyContent: 'space-around',
-                  alignItems: 'stretch',
-              }}
-          >
-              <View
-                  style={{
-                      backgroundColor: 'red',
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                  }}
-              >
-                  <Text>1</Text>
-              </View>
-              <View
-                  style={{
-                      backgroundColor: 'blue',
-                      flex: 2,
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                  }}
-              >
-                  <Text>2</Text>
-              </View>
-              <View
-                  style={{
-                      backgroundColor: 'green',
-                      flex: 3,
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                  }}
-              >
-                  <Text>3</Text>
-              </View>
-          </View>
-      </View>
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
 
-  );
+    const addGoalHandler = () => {
+        setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course Goal"
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button title="ADD" onPress={addGoalHandler} />
+            </View>
+            <View>
+                {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10
+    }
 });
